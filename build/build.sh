86df7e7b416478c20982fc77f637bfa85fca8a93
00ahui <00ahui@gmail.com>
2022-03-23 03:07:56 +0000

    0.1.1-swagger-gen

--- build/build.sh
+++ build/build.sh
@@ -3,12 +3,12 @@ echo "building ..."
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /tmp/flexlb-cli ../cmd/flexlb-client/main.go
 
 
-echo "copying ..."
+#echo "copying ..."
 
-DIR="/root/flexlb"
-TARGET="root@<TARGET>"
+#DIR="/root/flexlb"
+#TARGET="root@<TARGET>"
 
-scp /tmp/flexlb-cli ${TARGET}:${DIR}
+#scp /tmp/flexlb-cli ${TARGET}:${DIR}
 
 #PROXY="root@<PROXY>"
 #scp /tmp/flexlb-cli ${PROXY}:${DIR}
